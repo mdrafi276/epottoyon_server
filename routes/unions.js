7825ce800f405c978b12563181f39e762bf25dd7
EligibleDev <eligibledev@gmail.com>
2024-03-15 22:25:52 +0600

    add union info form's all get request done

--- routes/unions.js
+++ routes/unions.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const { unions } = require('../models');
 
 router.get('/', async (req, res) => {
-      const allUnions = await unions.findAll()
-      res.json(allUnions)
+      const {upazillaId} = req.query;
+
+      const matchedUnions = await unions.findAll({
+            where: {
+                  upazilla_id: upazillaId
+            }
+      })
+
+      res.json(matchedUnions)
 })
 
 module.exports = router
