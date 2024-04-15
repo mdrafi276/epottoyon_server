425b09d228e4de381d26d05a823c6dabc2d06eff
EligibleDev <eligibledev@gmail.com>
2024-04-15 07:37:34 +0600

    sanad type get api created

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,10 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { blogs } = require('../models');
+const { blogs } = require("../models");
+const { where } = require("sequelize");
 
-router.get('/', async (req, res) => {
-      const allBlogs = await blogs.findAll();
-      res.json(allBlogs);
-})
+router.get("/", async (req, res) => {
+    const allBlogs = await blogs.findAll();
+    res.json(allBlogs);
+});
 
+router.get("/get-type-by-id", async (req, res) => {
+    const { id } = req.query;
+
+    try {
+        const certificate = await blogs.findOne({
+            where: { id },
+            attributes: ["description"],
+        });
+        res.json(certificate);
+    } catch (error) {
+        res.status(500).json({
+            error: "An error occurred while fetching the certificate.",
+        });
+    }
+});
 module.exports = router;
