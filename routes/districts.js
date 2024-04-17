d8da9d582bfa606f632525435ffaa24bda9cd539
EligibleDev <eligibledev@gmail.com>
2024-04-17 20:55:57 +0600

    get district name by id api created

--- routes/districts.js
+++ routes/districts.js
@@ -1,15 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { districts } = require('../models');
+const { districts } = require("../models");
+const { where } = require("sequelize");
 
-router.get('/', async (req, res) => {
-      const { divisionId } = req.query;
-      const matchedDistricts = await districts.findAll({
+router.get("/", async (req, res) => {
+    try {
+        const { divisionId } = req.query;
+        const matchedDistricts = await districts.findAll({
             where: {
-                  division_id: divisionId
-            }
-      });
-      res.json(matchedDistricts);
-})
+                division_id: divisionId,
+            },
+        });
+        res.json(matchedDistricts);
+    } catch (error) {
+        console.error("Failed to fetch districts:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 
-module.exports = router
+router.get("/get-name-by-id", async (req, res) => {
+    try {
+        const { id } = req.query;
+
+        const district = await districts.findOne({
+            where: { id },
+            attributes: ["id", "name", "bn_name"],
+        });
+        res.json(district);
+    } catch (error) {
+        console.error("Failed to fetch district names:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+module.exports = router;
