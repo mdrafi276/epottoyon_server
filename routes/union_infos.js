359182beed5584315cecc7a4419bb06dba8db70f
EligibleDev <eligibledev@gmail.com>
2024-04-18 00:15:35 +0600

    unions info for pdf get api created

--- routes/union_infos.js
+++ routes/union_infos.js
@@ -1,26 +1,55 @@
 const express = require("express");
 const router = express.Router();
 const { union_infos } = require("../models");
+const { where } = require("sequelize");
 
 router.get("/", async (req, res) => {
-    const allUnions = await union_infos.findAll({
-        //   offset: 125,
-    });
-    res.json(allUnions);
+    try {
+        const allUnions = await union_infos.findAll();
+        res.json(allUnions);
+    } catch (error) {
+        res.status(500).send("Failed to retrieve unions");
+    }
 });
 
 router.post("/add-union", async (req, res) => {
-    const newUnion = req.body;
-    const result = await union_infos.create(newUnion);
-    res.send(result);
+    try {
+        const newUnion = req.body;
+        const result = await union_infos.create(newUnion);
+        res.send(result);
+    } catch (error) {
+        res.status(500).send("Failed to add new union");
+    }
 });
 
 router.get("/users-union", async (req, res) => {
-    const { userId } = req.query;
-    const union = await union_infos.findOne({
-        where: { user_id: userId },
-    });
-    res.send(union);
+    try {
+        const { userId } = req.query;
+        const union = await union_infos.findOne({
+            where: { user_id: userId },
+        });
+        if (!union) {
+            res.status(404).send("Union not found");
+        } else {
+            res.send(union);
+        }
+    } catch (error) {
+        res.status(500).send("Failed to retrieve union information");
+    }
+});
+
+router.get("/get-for-pdf", async (req, res) => {
+    try {
+        const { id } = req.query;
+        const unionInfo = await union_infos.findOne({
+            where: { id },
+            attributes: ["id", "union_id", "user_id", "chairman", "che"],
+        });
+
+        res.json(unionInfo);
+    } catch (error) {
+        res.status(500).send("Failed to retrieve union information");
+    }
 });
 
 module.exports = router;
