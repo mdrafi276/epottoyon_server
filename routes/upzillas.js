cbc0f2da0bf2e4a49509d6d6665641d2a6cf4532
EligibleDev <eligibledev@gmail.com>
2024-04-17 20:25:44 +0600

    handled errors in the get upazillas route

--- routes/upzillas.js
+++ routes/upzillas.js
@@ -1,16 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { upzillas } = require('../models');
+const { upzillas } = require("../models");
 
-router.get('/', async (req, res) => {
-      const { districtId } = req.query
-      const matchedUpzillas = await upzillas.findAll({
+router.get("/", async (req, res) => {
+    try {
+        const { districtId } = req.query;
+        const matchedUpzillas = await upzillas.findAll({
             where: {
-                  district_id: districtId
-            }
+                district_id: districtId,
+            },
+        });
+        res.json(matchedUpzillas);
+    } catch (error) {
+        console.error("Failed to fetch upzillas:", error);
+        res.status(500).send("An error occurred while fetching the upzillas.");
+    }
+});
+
+router.get("/get-name-by-unionId", async (req, res) => {
+    try {
+      const {unionId} = req.query;
+      const upzilla = await upzillas.findOne({
+            
       })
+    } catch (error) {
+        console.error("Failed to fetch upzilla names:", error);
+        res.status(500).send("An error occurred while fetching the upzilla names.");
+    }
+});
 
-      res.json(matchedUpzillas)
-})
-
-module.exports = router
+module.exports = router;
